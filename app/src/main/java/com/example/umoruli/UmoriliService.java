ec339c8accc51446c1588f1173d640c18e2b9189
m1khaelko <mihal.n03@gmail.com>
2021-12-04 03:10:20 +0900

    Initial commit

---
+++ app/src/main/java/com/example/umoruli/UmoriliService.java
@@ -0,0 +1,21 @@
+package com.example.umoruli;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+interface UmoriliService {
+
+    @GET("api/get")
+    Call<List<UPost>> getData(@Query("name") String resourceName, @Query("num") int count);
+
+    Retrofit retrofit = new Retrofit.Builder()
+            //.baseUrl("http://www.umori.li/")
+            .baseUrl("http://umorili.herokuapp.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+}
